c4dc8f98809e157cf477697a21c7b182ad2b9244
ismael2004-oo <alfredoquinchi50@gmail.com>
2025-10-02 21:34:57 -0500

    Se agrego una nuenva funcion que es saludar con el nombre que uno ingrese se va ver tanto en el log como el servidor

--- public/ismael.js
+++ public/ismael.js
@@ -10,5 +10,23 @@ function goodbye() {
   return mensaje;
 }
 
+function saludarNombre(nombre) {
+  var mensaje = "hola " + nombre;
+  console.log(mensaje);
+  return mensaje;
+}
+
+function saludarConNombre() {
+  var nombre = document.getElementById("nombreInput").value;
+
+  if (nombre == "") {
+    alert("Escriba algun nombre");
+    return;
+  }
+
+  var mensaje = saludarNombre(nombre);
+  document.getElementById("resultado").innerText = mensaje;
+}
+
 hello();
 goodbye();
